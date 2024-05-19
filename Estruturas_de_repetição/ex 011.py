0fbd3e82ed37c880bd6b7cf2a8da2125f143e5a2
ohhigordev <higor.nascimento0704@gmail.com>
2024-05-18 23:53:20 -0300

    Estruturas_de_dados

--- Estruturas_de_repetição/ex 011.py
+++ Estruturas_de_repetição/ex 011.py
@@ -3,14 +3,27 @@ Faça um programa que peça dois números, base e expoente, calcule e mostre o p
 Não utilize a função de potência da linguagem.
 '''
 
-def numeros_potencia():
-    while True:
-        num1 = int(input('Digite um número: '))
-        num2 = int(input('Digite o segundo número: '))
-        potencia = num1 ** num2
+def potencia(base, expoente):
+    resultado = 1
+    for _ in range(expoente):
+        resultado *= base
+    return resultado
 
-potencia_de_numeros = numeros_potencia
-print(potencia_de_numeros)
+
+def main():
+    # Solicitar a entrada do usuário para a base e expoente:
+    base = int(input('Digite a base: '))
+    expoente = int(input('Digite o expoente: '))
+
+    # Calcular a potência utilizando a função definida:
+    resultado = potencia(base, expoente)
+
+    # Exibe o resultado:
+    print(f'{base} elevado a {expoente} é igual a {resultado}')
+
+if __name__ == '__main__':
+    main()
+
 
 
 
---
+++ Estruturas_de_repetição/ex 012.py
@@ -0,0 +1,22 @@
+'''
+Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números 
+pares e a quantidade de números impares.
+'''
+
+# Pedindo os números ao usuário:
+while True:
+    n1 = int(input('Digite o primeiro número:'))
+    n2 = int(input('Digite o segundo número:'))
+    n3 = int(input('Digite o terceiro número:'))
+    n4 = int(input('Digite o quarto número:'))
+    n5 = int(input('Digite o quinto número:'))
+    n6 = int(input('Digite o sexto número:'))
+    n7 = int(input('Digite o setimo número:'))
+    n8 = int(input('Digite o oitavo número:'))
+    n9 = int(input('Digite o nono número:'))
+    n10 = int(input('Digite o decimo número:'))
+
+    
+
+
+
